0b8ce2f1a0f44af0bc8c2766fbdb5a269b16dd3e
EIoannou4 <eioannou4@uclan.ac.uk>
2019-11-17 19:24:36 +0200

    Functionality Code

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 function list() {
-    
+
+    fetch("https://codecyprus.org/th/api/list")
+        .then(response=> response.json())
+        .then(jsonObj =>{
+            let th = jsonObj.treasureHunts;
+            let uid = th[0].uuid;
+            for(let i=0; i<th.length; i++){
+                let listItem = document.createElement("li");
+                let username = document.getElementById("username").value;
+                listItem.innerHTML = "<a href='https://codecyprus.org/th/api/start?player="+username+"&app=Rocket&treasure-hunt-id=" + uid + "'>" + th[i].name + "</a>";
+                ch.appendChild(listItem);
+            }
+        });
 }
 
+
 function start() {
-    
+
+
+
 }
 
 function question() {
     
 }
+list();
